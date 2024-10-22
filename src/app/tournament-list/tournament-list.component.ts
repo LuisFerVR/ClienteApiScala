dcf3d9ff2df1e2ba91db72556f274647d31782f8
LuisFerVR <luis.fer.vera.ruiz@gmail.com>
2024-10-22 15:51:49 -0600

    Se agregó la selección de equipos en un torneo.

--- src/app/tournament-list/tournament-list.component.ts
+++ src/app/tournament-list/tournament-list.component.ts
@@ -1,24 +1,36 @@
-import { NgIf } from '@angular/common';
+import { NgFor, NgIf } from '@angular/common';
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-tournament-list',
   standalone: true,
-  imports: [FormsModule,NgIf],
+  imports: [FormsModule,NgIf, NgFor],
   templateUrl: './tournament-list.component.html',
   styleUrl: './tournament-list.component.css'
 })
 export class TournamentListComponent {
-  torneo = {
-    nombre: ''
-  };
+  nombre: string = '';
+  equiposSeleccionados: number[] = [];
+  equiposDisponibles: Array<{id:number, nombre:string}> = [
+    { id: 1, nombre: 'Equipo A' },
+    { id: 2, nombre: 'Equipo B' },
+    { id: 3, nombre: 'Equipo C' }
+  ];
 
   onSubmit(form: any) {
     if (form.valid) {
-      console.log('Torneo a registrar:', this.torneo);
-      // Aquí iría la lógica para guardar en la base de datos
-      form.reset();
+      const torneoEquipos = {
+        nombre: this.nombre,
+        equipos: this.equiposSeleccionados
+      };
+      
+      console.log('Torneo a registrar:', torneoEquipos);
+      
+      // Reseteamos el formulario completo
+      this.nombre = '';
+      this.equiposSeleccionados = [];
+      form.resetForm();
     }
   }
 }

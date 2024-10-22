9f5c7b3fa4f53e369f58503ee426954e4261f829
LuisFerVR <luis.fer.vera.ruiz@gmail.com>
2024-10-22 11:21:26 -0600

    Se construyeron todos los componentes necesarios y previstos, se agregarón rutas con carga perezosa, se instaló y agregó al archivo angular.json Bootstrap v5.3, se hizo el home y header component.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,22 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { HeaderComponent } from './header/header.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        component: HeaderComponent
+    },
+    {
+        path: 'equipo',
+        loadComponent:()=>import('./club-list/club/club.component').then(m=>m.ClubComponent)
+    },
+    {
+        path: 'jugador',
+        loadComponent:()=>import('./club-list/club/club.component').then(m=>m.ClubComponent)
+    },
+    {
+        path: 'torneo',
+        loadComponent:()=>import('./club-list/club/club.component').then(m=>m.ClubComponent)
+    }
+];
---
+++ src/app/club-list/club-list.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-club-list',
+  standalone: true,
+  imports: [],
+  templateUrl: './club-list.component.html',
+  styleUrl: './club-list.component.css'
+})
+export class ClubListComponent {
+
+}
